cb68658ae106f0457ee2482df4ce7f3d88cd1b27
Stephanie <stephaniiehallberg@gmail.com>
2022-01-31 14:42:06 +0100

    add redis to posts

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,3 +1,4 @@
+import * as redisStore from 'cache-manager-ioredis';
 import { CacheModule, Module } from '@nestjs/common';
 import PostsController from './posts.controller';
 import PostsService from './posts.service';
@@ -5,9 +6,23 @@ import Post from './post.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SearchModule } from '../search/search.module';
 import PostsSearchService from './postsSearch.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [CacheModule.register({ ttl: 5, max: 100 }), TypeOrmModule.forFeature([Post]), SearchModule],
+  imports: [
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        store: redisStore,
+        host: configService.get('REDIS_HOST'),
+        port: configService.get('REDIS_PORT'),
+        ttl: 120,
+      }),
+    }),
+    TypeOrmModule.forFeature([Post]),
+    SearchModule,
+  ],
   controllers: [PostsController],
   providers: [PostsService, PostsSearchService],
 })
