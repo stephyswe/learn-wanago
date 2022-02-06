0c3a884c6877a6528154175ec856f8e23a6676ee
Stephanie <stephaniiehallberg@gmail.com>
2022-02-06 16:09:33 +0100

    documentation (cat example)

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -8,15 +8,27 @@ import CategoryNotFoundException from './exceptions/categoryNotFound.exception';
 
 @Injectable()
 export default class CategoriesService {
+  /**
+   * @ignore
+   */
   constructor(
     @InjectRepository(Category)
-    private categoriesRepository: Repository<Category>
+    private categoriesRepository: Repository<Category>,
   ) {}
 
-  getAllCategories() {
+  /**
+   * A method that fetches the categories from the database
+   * @returns A promise with the list of categories
+   */
+  getAllCategories(): Promise<Category[]> {
     return this.categoriesRepository.find({ relations: ['posts'] });
   }
 
+  /**
+   * A method that fetches a category with a given id. Example:
+   * @example
+   * const category = await categoriesService.getCategoryById(1);
+   */
   async getCategoryById(id: number) {
     const category = await this.categoriesRepository.findOne(id, { relations: ['posts'] });
     if (category) {
@@ -31,19 +43,33 @@ export default class CategoriesService {
     return newCategory;
   }
 
-  async updateCategory(id: number, category: UpdateCategoryDto) {
+  /**
+   * See the [definition of the UpdateCategoryDto file]{@link UpdateCategoryDto} to see a list of required properties
+   */
+  async updateCategory(id: number, category: UpdateCategoryDto): Promise<Category> {
     await this.categoriesRepository.update(id, category);
     const updatedCategory = await this.categoriesRepository.findOne(id, { relations: ['posts'] });
     if (updatedCategory) {
-      return updatedCategory
+      return updatedCategory;
     }
     throw new CategoryNotFoundException(id);
   }
 
-  async deleteCategory(id: number) {
+  /**
+   * A method that deletes a category from the database
+   * @param id An id of a category. A category with this id should exist in the database
+   */
+  async deleteCategory(id: number): Promise<void> {
     const deleteResponse = await this.categoriesRepository.delete(id);
     if (!deleteResponse.affected) {
       throw new CategoryNotFoundException(id);
     }
+  }
+
+  /**
+   * @deprecated Use deleteCategory instead
+   */
+  async deleteCategoryById(id: number): Promise<void> {
+    return this.deleteCategory(id);
   }
 }
